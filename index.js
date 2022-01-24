03be1d621404ce68173b50d70e74e130e627c212
RyanDeLong <rdelong111@gmail.com>
2022-01-24 13:39:16 -0600

    commit tree 20013b064e73836c4d814340f662f7bda54b8fe0

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // return word === word.split('').reverse().join('');
+
+  const len = word.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  return condition(word equals to word((split into array)(reverse the array)(join array in a string))
+
+  SECOND SOLUTION:
+  Set constant to length of word called len.
+
+  iterate over half the length of the word
+    if a letter in word does not equal to another letter in word
+      return false
+
+  return true
 */
 
 /*
-  Add written explanation of your solution here
+  I took the string input and then split the string into an array by each letter.
+  The array is then reversed.
+  The array is then joined back into a string.
+  I returned a condition that compared the original string with the reversed.
+
+  SECOND SOLUTION;
+  I first set the length of word to a constant to help with clean code later.
+  A for loop that iterates through half the length of word is set.
+  Inside the for loop, an if statement checks if a letter does not equal to the other letter at the opposite end of the word. 
+  If the IF statement is true, then it returns false.
+  If the for loop runs through all the way, then the function returns true.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +47,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("aabbbccccddddccccbbbaa"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("asdsakdgjfmrbmrjhskdfh"));
 }
 
 module.exports = isPalindrome;
